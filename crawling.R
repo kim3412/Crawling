f60b39075c99552aa0d25d4b245f034a927ae008
kim3412 <49008670+kim3412@users.noreply.github.com>
2019-06-13 01:17:30 +0900

    Update crawling.R

--- crawling.R
+++ crawling.R
@@ -1,38 +1,40 @@
 library(RSelenium)
-
-# set logfile name as yesterday's date
+library(data.table)
+# get yesterday's date
 ydate = Sys.Date() - 1
 ydate = as.character(ydate)
-logFileName = paste0("D:/scrap/", ydate, ".txt")
-# sink
-tt = file("D:/scrap/0507.txt", open="at")
+tdate = as.character(Sys.Date())
+# set log file
+logFileName = paste0("D:/scrap/", ydate, ".txt", sep="")
+tt = file(logFileName, open="at")
 sink(tt, type=c("output", "message"), append=TRUE)
 
 ####### setting browser
 pjs <- wdman::phantomjs()
 Sys.sleep(5)
+### neeeeeeed some time # Sys.sleep(time in sec)
 eCap <- list(phantomjs.page.settings.userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20120101 Firefox/29.0")
 remDr <- remoteDriver(browserName = "phantomjs", port = 4567L, extraCapabilities=eCap)
-remDr$open()
+remDr$open() ### sometimes failure
 
 
 ####### login to naver
 remDr$navigate("https://nid.naver.com/nidlogin.login")
 
 id = remDr$findElement(using="id", value="id")
-id$setElementAttribute("value", "Input your id")
+id$setElementAttribute("value", "yourId")
 Sys.sleep(3)
 
 pw = remDr$findElement(using="id", value="pw")
-pw$setElementAttribute("value", "Input your pw")
+pw$setElementAttribute("value", "yourPW")
 Sys.sleep(3)
 
 loginBtn = remDr$findElement(using="css", value=".btn_global")
 loginBtn$clickElement()
 
 
-# use url directly
-pageUrl = "https://cafe.naver.com/ArticleSearchList.nhn?search.clubid=10050146&search.media=0&search.searchdate=2019-05-062019-05-06&search.defaultValue=1&search.exact=&search.include=&userDisplay=50&search.exclude=&search.option=0&search.sortBy=date&search.searchBy=0&search.searchBlockYn=0&search.includeAll=&search.query=%BE%C6%C0%CC%C6%D0%B5%E5&search.viewtype=title&search.page=1"
+# use results directly
+pageUrl = "https://cafe.naver.com/ArticleSearchList.nhn?search.clubid=10050146&search.media=0&search.searchdate=2019-05-062019-05-06&search.defaultValue=1&userDisplay=50&search.option=0&search.sortBy=date&search.searchBy=1&search.searchBlockYn=0&search.query=%BE%C6%C0%CC%C6%D0%B5%E5&search.viewtype=title&search.page=1"
 
 # modify date
 mDate = paste0("searchdate=", ydate, ydate, "&search.defaultValue", sep="")
@@ -40,8 +42,10 @@ pageUrl = gsub("searchdate=.*&search.defaultValue", mDate, pageUrl)
 
 
 ####### get yesterday's posts
-ydf = data.frame(matrix(nrow=0, ncol=10))
+ydf = data.frame(matrix(nrow=0, ncol=11))
+ydf1 = data.frame(matrix(nrow=0, ncol=10))
 page = 1
+
 while(TRUE){
   tryCatch({
     
@@ -57,48 +61,36 @@ while(TRUE){
     ### sales status, price are not always exists(so handle exception)
     
     # get posts list
-    postList = remDr$findElements(using="css", value="#main-area  > div:nth-child(5) > table > tbody > tr")
     
-    # variables
-    boardNumVec = c()
-    nickVec = c()
-    numOfViewsVec = c()
-    urlVec = c()
-    titleVec = c()
+    postList = remDr$findElements(using="css", value="#main-area  > div:nth-child(5) > table > tbody > tr")
+    ### postList list
     saleStatusVec = c()
     priceVec = c()
     sellerVec = c()
+    contentVec = c()
+    commentVec = c()
     
-    for (i in 1:length(postList)){
-      print(i)
-      
-      # boardNumVec and urlVec
-      tmp = postList[[i]]$findChildElement(using="css", value="td.td_article div.inner_number")
-      tmpText = as.character(tmp$getElementText())
-      boardNumVec[i] = tmpText
-      urlVec[i] = paste0("https://cafe.naver.com/joonggonara/", tmpText, sep="")
-      print(tmpText)
-      
-      # nickVec
-      tmp = postList[[i]]$findChildElement(using="css", value="td.td_name a")
-      tmpText = as.character(tmp$getElementText())
-      nickVec[i] = tmpText
-      print(tmpText)
-      
-      # number of views
-      tmp = postList[[i]]$findChildElement(using="css", value="td.td_view")
-      tmpText = as.character(tmp$getElementText())
-      numOfViewsVec[i] = tmpText
-      print(tmpText)
-      
-      # titleVec
-      tmp = postList[[i]]$findChildElement(using="css", value="td.td_article div.inner_list > a")
-      tmpText = as.character(tmp$getElementText())
-      titleVec[i] = tmpText
-      print(tmpText)
-      
-      Sys.sleep(sample(4:8, 1))
-    }
+    # boardNumVec and urlVec
+    tmp = sapply(postList, function(x){x$findChildElement(using="css", value="td.td_article div.inner_number")})
+    tmpText = sapply(tmp, function(x){as.character(x$getElementText())})
+    urlVec = sapply(tmpText, function(x){paste0("https://cafe.naver.com/joonggonara/", x, sep="")})
+    print("urlVec")
+    
+    # nickVec
+    tmp = sapply(postList, function(x){x$findChildElement(using="css", value="td.td_name a")})
+    nickVec = sapply(tmp, function(x){as.character(x$getElementText())})
+    print("nickVec")
+    
+    # number of views
+    tmp = sapply(postList, function(x){x$findChildElement(using="css", value="td.td_view")})
+    numOfViewsVec = sapply(tmp, function(x){as.character(x$getElementText())})
+    print("numOfViewsVec")
+    
+    # titleVec
+    tmp = sapply(postList, function(x){x$findChildElement(using="css", value="td.td_article div.inner_list > a")})
+    titleVec = sapply(tmp, function(x){as.character(x$getElementText())})
+    print("titleVec")
+    
     
     for(i in 1:length(urlVec)){
       
@@ -129,8 +121,22 @@ while(TRUE){
         # priceVec
         tmp = remDr$findElement(using="css", value=".cost")
         tmpText = as.character(tmp$getElementText())
+        tmpText = gsub(',', '', tmpText)
         priceVec[i] = tmpText
         print(tmpText)
+        
+        # contents
+        tbody = remDr$findElement(using="id", value="tbody")
+        textTbody = tbody$getElementText()
+        textTbody = paste(unlist(textTbody), collapse='')
+        contentVec[i] = textTbody
+        
+        # comments
+        commentList = remDr$findElements(using="css", value="#cmt_list > li > div > p")
+        if(!is.null(commentList)){
+          tmp = unlist(sapply(commentList, function(x){x$getElementText()}))
+          commentVec[i] = paste(tmp, collapse = '|')
+        } else commentVec[i] = NA
       },
       error = function(e){
         saleStatusVec[i] = NA
@@ -138,22 +144,21 @@ while(TRUE){
         priceVec[i] = NA
         print(e)
       })
-      
-      Sys.sleep(sample(8:11, 1))
     }
     
+    # combine the infomation on each vector into datframe ydf
+    ydf = rbind(ydf, cbind(urlVec, nickVec, numOfViewsVec, titleVec, saleStatusVec, priceVec, sellerVec, contentVec, commentVec))
   },
   error = function(e){
     merror = paste0("fail to get list ", pageUrl)
     print(merror)
     print(e)
-    
+    break
   },
   finally = {
-    # combine the infomation on each vector into datframe ydf
-    ydf = rbind(ydf, cbind(boardNumVec, urlVec, nickVec, numOfViewsVec, titleVec, saleStatusVec, priceVec, sellerVec))
     
-    if(length(postList) != 50) break
+    # sometimes even if it is not in the end of list, it stops
+    # maybe have some problems during get postList and can't get 50 trs.
     page = page + 1
     pageNum = paste0("search.page=", page, sep="")
     pageUrl = gsub("search.page=.*", pageNum, pageUrl)
@@ -162,15 +167,21 @@ while(TRUE){
   
 }
 
-# add posting date and sales end date to the data frame
-# then store data frame
-tdate = format(Sys.Date()-1, "%Y.%m.%d.")
-dateVec = rep(tdate, nrow(ydf))
+tydate = gsub('-', '', ydate)
+sdate = paste0(gsub('-', '.', ydate), '.')
+dateVec = rep(sdate, nrow(ydf))
 edate = ifelse(ydf$saleStatusVec == "완료", tdate, NA)
-ydf = cbind(ydf, tdate, edate)
-write.table(ydf, "D:/scrap/scrapInfo.csv", sep=",", append=TRUE, na="NA", row.names = FALSE, col.names = FALSE)
+ydf = cbind(ydf, sdate, edate)
+names(ydf) = c("url", "nick", "views", "title", "sale", "price", "d", "content", "comment", "sdate", "edate")
+ydf1 = subset(ydf, select=-c(content, comment))
 
-# relase the sink
+fname = paste0("D:/scrap/scrapInfo", tydate, ".csv")
+fname1 = paste0("D:/scrap/scrapInfo", tydate, "t.csv")
+write.table(ydf1, fname, sep=",", append=TRUE, na="NA", row.names = FALSE, col.names = FALSE)
+
+write.table(ydf, fname1, sep=",", append=TRUE, na="NA", row.names = FALSE, col.names = FALSE)
+
+ydf$본문내용
 sink()
 ####### close
 remDr$close() 
