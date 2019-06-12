b733cea89eabef41cdde8a946f6e2aaa8a2fc2eb
kim3412 <49008670+kim3412@users.noreply.github.com>
2019-06-13 03:01:29 +0900

    Update preprocessing.R

--- preprocessing.R
+++ preprocessing.R
@@ -1,10 +1,13 @@
 ############### function definition
-### preprocess data by using title
-titlePreprocess <- function(reg_expr, data, contain = TRUE) {
-  index = grepl(reg_expr, data$title, ignore.case=TRUE)
+
+##### data cleaning
+
+### preprocess title of given dataframe
+titlePreprocess <- function(reg_expr, mdata, contain = TRUE) {
+  index = grepl(reg_expr, mdata$title, ignore.case=TRUE)
   if(!contain)
     index = !index
-  return (subset(data, index))
+  return (subset(mdata, index))
 }
 
 ### exclude not needed data
@@ -45,14 +48,181 @@ Preprocess <- function(fname){
   return (adf)
 }
 
+#----------------------------------------------------------------#
+
+##### extract infomation from title
+
+### extract only one attribute
+# It is considered an error if there are multiple attributes
+# use this function to extract display, connect, storage
+# cause price information is meaningless when different types exist
+extractOne <- function(reg_expr, ex, err_msg){
+  result = c()
+  for(i in 1:nrow(ex))
+  {
+    tmp = gregexpr(reg_expr, ex[i, 4], ignore.case=TRUE)
+    if(length(tmp[[1]]) > 1){
+      result[i] = err_msg
+    } else if(tmp[[1]] == -1){
+      result[i] = NA
+    } else result[i] = substr(ex[i, 4], tmp[[1]], tmp[[1]]+attr(tmp[[1]], "match.length")-1)
+  }
+  return (result)
+}
+
+### extract connect attribute
+# change the various expressions of sellers
+# to be uniform
+extractConnect <- function(reg_expr, ex, err_msg){
+  connect = extractOne(reg_expr, ex, err_msg)
+  connect = gsub("셀룰?롤?러?|lte", "cellular", connect)
+  connect = gsub("와이?파이?|wi-?fi", "wifi", connect)
+  return (connect)
+}
+
+
+### extract all attributes
+# use this function to extract color, service
+extractAll <- function(reg_expr, attrC, ex){
+  result = c()
+  for(i in 1:nrow(ex)){
+    acc = ""
+    for(j in 1:length(reg_expr)){
+      if(grepl(reg_expr[j], ex[i, 4])) acc = paste0(acc, attrC[j], sep = "|")
+    }
+    if(acc == "") acc = NA
+    result[i] = acc
+  }
+  return (result)
+}
+
+### extract state
+# more states need to be handled
+# eg. s급, ~사용, 이상, 고장, 부품용 등
+extractState <- function(ex){
+  state= c()
+  stateC = c("풀박스", "리퍼제품", "미개봉", "새상품")
+  stateR = c("풀박", "리퍼\\s?후?\\s?(미개봉|새\\s?상품|새\\s?제품|미사용|신품)", "미개봉", "새\\s?상품|새\\s?제품|새\\S?거임|새\\s?것임")
+  for(i in 1:nrow(ex)){
+    acc = ""  
+    for(at in 1:length(stateC)){
+      tmp = regexpr(stateR[at], ex[i, 4])
+      if(tmp[1] != -1){
+        acc = stateC[at]
+        break
+      }
+    }
+    if(acc == "") acc = NA
+    state[i] = acc
+  }
+  return (state)
+}
+
+### extract whether a characteristic exists
+# use this function to extract pencil
+extractExist <- function(reg_expr, ex){
+  result = c()
+  for(i in 1:nrow(ex)){
+    tmp = regexpr(reg_expr, ex[i, 4], ignore.case=TRUE)
+    if(tmp[1] == -1){
+      result[i] = 0
+    } else result[i] = 1
+  }
+  return (result)
+}
+
+### extract whether a keyboard is included
+# include keyboard
+# exclude keyboard case
+extractKeyboard <- function(ex){
+  keyboard = c()
+  for(i in 1:nrow(ex)){
+    reg_expr = "키보드"
+    tmp = regexpr(reg_expr, ex[i, 4], ignore.case=TRUE)
+    if(tmp[1] == -1){
+      keyboard[i] = 0
+    } else{
+      reg_expr = "키보드\\s?케이스"
+      tmp = regexpr(reg_expr, ex[i, 4], ignore.case=TRUE)
+      if(tmp[1] == -1){
+        keyboard[i] = 1
+      } else keyboard[i] = 0
+    } 
+  }
+  return (keyboard)
+}
+
+### generate regular expressions
+### for the color of the device
+colorReg <- function(opts){
+  color = ""
+  if(grepl("실버", opts))
+    color = paste0(color, "실버|silver|화이트|white", '@')
+  if(grepl("스페이스 그레이", opts))
+    color = paste0(color, "스페?이?스?그레?이?|space|gray|sg", '@')
+  if(grepl("로즈골드", opts))
+    color = paste0(color, "로즈\\s?골드|로즈\\s?핑크|rose\\s?gold|pink", '@')
+  if(grepl("골드", opts))
+    color = paste0(color, "골드|gold", '@')
+  color = substr(color, 1, nchar(color)-1)
+  return (color)
+}
+
+
+### make reg_expr list
+# give the options by type as a parameter
+# return a vector of regular expressions
+options <- function(opts){
+  reg_expr = c()
+  # display size
+  reg_expr[1] = opts[1]
+  # connection method
+  reg_expr[2] = "셀룰?롤?러?|lte|와이?파이?|wi-?fi"
+  # storage size
+  reg_expr[3] = opts[2]
+  # color
+  reg_expr[4] = colorReg(opts[3])
+  # service
+  reg_expr[5] = "리퍼.*남음?은?|리퍼\\s?남아\\s?있음|리퍼\\s?가능|리퍼.*까지|리퍼.*년|리퍼\\s?.*\\d{2}[-/.]*(0?[1-9]|1[012])[-/.]*@케어|케플"
+  # pencil
+  reg_expr[6] = "pencil|펜슬|팬슬"
+  
+  return (reg_expr)
+}
+
+### obtain attribute values
+### and combine them into a single dataframe
+makeDataFrame <- function(reg_expr, attr_expr, mdata){
+  display = extractOne(reg_expr[1], mdata, -1)
+  connect = extractConnect(reg_expr[2], mdata, "Error")
+  
+  storage = extractOne(reg_expr[3], mdata, -1)
+  storage = gsub("1\\s?테라|1tb?", 1024, storage, ignore.case=TRUE)
+  
+  regC = unlist(strsplit(reg_expr[4], "@"))
+  attrC = unlist(strsplit(attr_expr[1], "@"))
+  color = extractAll(regC, attrC, mdata)
+  
+  regC = unlist(strsplit(reg_expr[5], "@"))
+  attrC = unlist(strsplit(attr_expr[2], "@"))
+  service = extractAll(regC, attrC, mdata)
+  
+  state = extractState(mdata)
+  
+  pencil = extractExist(reg_expr[6], mdata)
+  
+  keyboard = extractKeyboard(mdata)
+  
+  return (cbind(display, connect, storage, color, service, state, pencil, keyboard))
+}
+
 ### extract pro types from data 
 ### and extract information from pro types
 ### then save
-extractProAndSave <- fucntion(data){
-  ## extract pro types from data
+extractProAndSave <- function(mdata){
   # pro
   reg_expr = "프로|pro/i"
-  pro = titlePreprocess(reg_expr, data)
+  pro = titlePreprocess(reg_expr, mdata)
   # 3세대
   # 애플펜슬 2세대는 3세대와만 호환, 프로 3세대를 그냥 프로로 기재하고 펜슬 2세대와 함께 파는 경우 존재
   reg_expr = "3세대|프로3|펜슬\\s?2"
@@ -65,82 +235,46 @@ extractProAndSave <- fucntion(data){
   pro2 = titlePreprocess(reg_expr, pro)
   pro = titlePreprocess(reg_expr, pro, FALSE)
   
-  ## extract information from pro types
-  # display
-  reg_expr = "11|12.9"
-  tmp = gregexpr(reg_expr, ex)
-  # 12.9형과 11형을 모두 팔 경우 에러로 표시(-1)
-  if(length(tmp[[1]]) > 1){
-    display = -1
-  } else if(tmp[[1]] == -1){
-    display = NA
-  } else display = substr(ex, tmp[[1]], tmp[[1]]+attr(tmp[[1]], "match.length")-1)
+  # 기종별 게시글 수
+  numOfPosts = c(nrow(pro), nrow(pro2), nrow(pro3))
 
-  # connect
-  # lte, LTE, 셀룰러, 셀룰, 셀롤러
-  # wifi, wi-fi, Wi-Fi, WiFi, WIFI, 와이파이, 와파
-  reg_expr = "(:?셀룰?롤?러?|lte|와이?파이?|wi-?fi)"
-  #|wi/i-?fi/i|와이?파이?)"
-  tmp = gregexpr(reg_expr, ex, ignore.case=TRUE)
-  if(length(tmp[[1]]) > 1){
-    connect = "Error"
-  } else if(tmp[[1]] == -1){
-    connect = NA
-  } else connect = substr(ex, tmp[[1]], tmp[[1]]+attr(tmp[[1]], "match.length")-1)
+  # 3세대 데이터 프레임 추출하고 저장하기
+  pro3AttrReg = options(c("11|12.9", "64|256|512|1\\s?테라|1tb?", "실버 스페이스 그레이"))
+  pro3Attr = c("실버@스페이스 그레이", "리퍼@애플케어플러스")
+  pro3DF = makeDataFrame(pro3AttrReg, pro3Attr, pro3)
+  pro3DF = cbind(pro3DF, subset(pro3, select=c("price", "title", "url")))
+  write.table(pro3DF, "D:/scrap/pro3.csv", sep=",", append=TRUE, na="NA", row.names = FALSE, col.names = FALSE)
 
-  # storage
-  # pro3의 경우 64, 256, 512, 1024 기가
-  reg_expr = "64|256|512|1\\s?테라|1tb?"
-  tmp = gregexpr(reg_expr, ex, ignore.case=TRUE)
-  if(length(tmp[[1]]) > 1){
-    storage = -1
-  } else if(tmp[[1]] == -1){
-    storage = NA
-  } else storage = substr(ex, tmp[[1]], tmp[[1]]+attr(tmp[[1]], "match.length")-1)
+  # 2세대 데이터 프레임 추출하고 저장하기
+  pro2AttrReg = options(c("10.5|12.9", "64|256|512", "실버 스페이스 그레이 로즈골드 골드"))
+  pro2Attr = c("실버@스페이스 그레이@로즈골드@골드", "리퍼@애플케어플러스")
+  pro2DF = makeDataFrame(pro2AttrReg, pro2Attr, pro2)
+  pro2DF = cbind(pro2DF, subset(pro2, select=c("price", "title", "url", "sdate")))
+  write.table(pro2DF, "D:/scrap/pro2.csv", sep=",", append=TRUE, na="NA", row.names = FALSE, col.names = FALSE)
 
-  if(grepl("1", storage)) storage = 1024
-
-
-  # color
-  # pro3의 경우 실버와 스그
-  color = ""
-  reg_expr = "실버|silver|화이트|white"
-  if(grepl(reg_expr, ex)) color = paste(color, "실버", sep="|")
-
-  reg_expr = "스페?이?스?그레?이?|space|gray|sg"
-  if(grepl(reg_expr, ex)) color = paste(color, "스페이스 그레이", sep="|")
-
-  if(color == "") color = NA
-
-  # state
-  # 미개봉
-  # 새상품
-  # 리퍼신품
-  # s급
-  # aa급
-  # 고장, 이상, 찍힘, 눌림
-  # ~ 사용 ~
-  # 못 할 것 같은데 말이지...
-
-  # 펜슬
-  reg_expr = "펜슬|pencil"
-  tmp = regexpr(reg_expr, ex, ignore.case=TRUE)
-  if(tmp[1] == -1){
-    pencil = 0
-  } else pencil = 1
-
-  # 키보드
-  # folio 키보드, 정품 키보드, 정품 스마트 키보드, 스마트 폴리오 키보드, 애플 스마트 키보드
-  # 로지텍(logitech) ... 키보드
-  # 벨킨(belkin) ... 키보드
-  # 기타 키보드: 무선 키보드, 블루투스 키보드
-  # 키보드 케이스는 포함되면 안됨
-  reg_expr = "폴리오|folio|스마트\\s?키보드|로지텍\\s?키보드|logitech\\s?키보드|벨킨\\s?키보드|belkin\\s?키보드|무선\\s?키보드|블루투스\\s?키보드"
-  tmp = regexpr(reg_expr, ex, ignore.case=TRUE)
-  if(tmp[1] == -1){
-    keyboard = 0
-  } else keyboard = 1
-
+  # 1세대 데이터 프레임 추출하고 저장하기
+  proAttrReg = options(c("9.7|12.9", "32|128|256", "실버 스페이스 그레이 로즈골드 골드"))
+  proAttr = c("실버@스페이스 그레이@로즈골드@골드", "리퍼@애플케어플러스")
+  proDF = makeDataFrame(proAttrReg, proAttr, pro)
+  proDF = cbind(proDF, subset(pro, select=c("price", "title", "url")))
+  write.table(proDF, "D:/scrap/pro.csv", sep=",", append=TRUE, na="NA", row.names = FALSE, col.names = FALSE)
+  return (numOfPosts)  
 }
 
-t = Preprocess("D:/scrap/scrapInfo20190528.csv")
+# make date vector
+may = paste0("05", c(20:21, 23:26, 28, 30, 31))
+june = paste0("060", c(1, 2, 4:9))
+june = c(june, paste0("06", c(10:11)))
+dateV = c(may, june)
+fnameV = paste0("D:/scrap/scrapInfo2019", dateV, ".csv")
+
+dailyPost = data.frame(matrix(nrow=0, ncol = 5))
+for(i in 1:length(fnameV)){
+  print(dateV[i])
+  t = Preprocess(fnameV[i])
+  tmp = extractProAndSave(t)
+  print(tmp)
+  dailyPost = rbind(dailyPost, c(nrow(t), tmp))
+}
+dailyPost = cbind(dateV, dailyPost)
+write.table(dailyPost, "D:/scrap/dailyPost.csv", sep=",", append=TRUE, na="NA", row.names = FALSE, col.names = FALSE)
